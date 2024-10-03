ca3c633a33756e160393bdd518b5dd953618b70d
Armando Ruiz <armando.ruiz@industrie.co>
2024-10-03 11:45:30 +1000

    creado el types de los todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { useState } from "react"
+import { Todos } from "./components/Todos"
+
+const mockTodos = [
+  {
+    id: "1",
+    title: 'todo 1',
+    completed: false
+  },
+  {
+    id: "2",
+    title: 'todo 2',
+    completed: false
+  },
+  {
+    id: "3",
+    title: 'todo 3',
+    completed: false
+  }
+]
+
 const App = () => {
+  const [todos, setTodos] = useState(mockTodos)
 
   return (
     <>
-      <h1>TODO mvc</h1>
+      <Todos todos={todos} />
     </>
   )
 }
