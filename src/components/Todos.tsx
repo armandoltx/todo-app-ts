215be2f34fbf11cf05d796494bf96227f6b3dfb7
Armando Ruiz <armando.ruiz@industrie.co>
2024-10-03 13:51:09 +1000

    creado el componente todo

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,17 +1,24 @@
 import type { ListOfTodos } from "../types"
+import { Todo } from "./Todo"
 
 interface Props {
   todos: ListOfTodos
 }
 
-export const Todos: React.FC<Props> = ({ todos}) => {
+export const Todos: React.FC<Props> = ({ todos }) => {
   return(
     <ul>
       {todos.map(todo => (
         <li
           key={todo.id}
+          className={`${todo.completed ? 'completed' : ''}`}
         >
-            {todo.title}
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            completed={todo.completed}
+          />
         </li>
       ))}
     </ul>
